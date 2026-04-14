9fe9347a699cd95a1730ef7939d22f1ee319208f
Theo Lemaire <theolemaire@MacBook-Air-de-Theo.local>
2026-04-14 07:41:01 -0300

    feat: create template for connection/register and book list/creation

--- books/views.py
+++ books/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+
+@login_required
+def book_create(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        author = request.POST['author']
+        description = request.POST['description']
+
+        Book.objects.create(
+            title=title,
+            author=author,
+            description=description,
+            owner=request.user
+        )
+        return redirect('book_list')
+
+    return render(request, 'books/book_form.html')
+
+
+@login_required
+def book_update(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.user != book.owner:
+        return redirect('book_list')
+
+    if request.method == 'POST':
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.description = request.POST['description']
+        book.save()
+        return redirect('book_list')
+
+    return render(request, 'books/book_form.html', {'book': book})
+
+
+@login_required
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.user == book.owner:
+        book.delete()
+
+    return redirect('book_list')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # connecte automatiquement l'utilisateur
+            return redirect('book_list')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'

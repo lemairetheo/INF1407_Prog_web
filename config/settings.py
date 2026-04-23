937c52e3bce555e6908df88a179d7ba8ebc5ef08
lemairetheo <114654759+lemairetheo@users.noreply.github.com>
2026-04-24 01:20:16 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management import call_command
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'book_list'
 LOGOUT_REDIRECT_URL = 'book_list'
+
+try:
+    call_command('migrate')
+except Exception as e:
+    print(e)
